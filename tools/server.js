842eeb9ee8be9d1d568f9107f7b4f1ee8c9be9a1
Glendon Cheney <glendon.cheney@gmail.com>
2017-07-28 18:15:20 -0500

    Setup initial app

---
+++ tools/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import webpack from 'webpack';
+import path from 'path';
+import config from '../webpack.config.dev';
+import open from 'open';
+import morgan from 'morgan';
+
+/* eslint-disable no-console */
+
+const port = 3000;
+const app = express();
+const compiler = webpack(config);
+
+morgan(':method :url :status :res[content-length] - :response-time ms');
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join( __dirname, '../src/index.html'));
+});
+
+app.listen(port, function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        open('http://localhost:' + port);
+    }
+});
